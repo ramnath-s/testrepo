added6c312f1e8e94bbdd21f1366bec2ecfc64b8
rgs2 <rgs2@DESKTOP-DETG9CC>
2021-06-20 11:23:19 +0530

    feb21

---
+++ Feb21/src/main/scala/handson/task2.scala
@@ -0,0 +1,5 @@
+package handson
+
+object task2 {
+  
+}
---
+++ Feb21/src/main/scala/tasks/tasks.scala
@@ -0,0 +1,11 @@
+package tasks
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+
+object tasks {
+  
+}
